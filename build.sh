42715a6498e67b10b7af1261fcf2b4547ca056a4
Ethan Frei <ethan.frei@gmail.com>
2015-04-17 18:47:02 +0000

    updating build script

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-avr-gcc -mmcu=attiny44a -DF_CPU=1000000UL -O -o main.bin main.c && \
-avr-objcopy -O ihex main.bin main.hex && \
-avrdude -c usbtiny -p t44 -U flash:w:main.hex
+mkdir bin
 
-rm main.hex main.bin
+avr-gcc -mmcu=attiny44a -DF_CPU=1000000UL -O -o bin/main.bin src/main.c && \
+avr-objcopy -O ihex bin/main.bin bin/main.hex && \
+avrdude -c usbtiny -p t44 -U flash:w:bin/main.hex
+
+rm -rf bin
 
 # avrdude -c usbtiny -p t44 -U lfuse:w:0x62:m -U hfuse:w:0x5f:m
