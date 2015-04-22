04b66682601b24123b726f69f2eaf24ed67cd456
Ethan Frei <ethan.frei@gmail.com>
2015-04-22 22:39:07 +0000

    updates

--- src/main.c
+++ src/main.c
@@ -4,110 +4,56 @@
                           // _delay_us()
 #include <avr/interrupt.h>
 
-volatile uint8_t isDisplayingTensDigit = 0;
-volatile uint8_t tempToDisplay = 0;
+volatile uint8_t doorOpen = 0;
+volatile uint8_t flagUp = 0;
+volatile uint8_t beamDetected = 0;
 
 
-void digitToDisplay(uint8_t digit) {
-    if(digit > 9) {
-        return;
-    }
-    uint8_t aFilter = PORTA & 0x1B;
-    uint8_t bFilter = PORTB & 0xF0;
-    switch(digit) {
-        case 0:
-            PORTA = aFilter | 0x00;
-            PORTB = bFilter | 0x04;
-            break;
-        case 1:
-            PORTA = aFilter | 0xC0;
-            PORTB = bFilter | 0x07;
-            break;
-        case 2:
-            PORTA = aFilter | 0x84;
-            PORTB = bFilter | 0x00;
-            break;
-        case 3:
-            PORTA = aFilter | 0x80;
-            PORTB = bFilter | 0x01;
-            break;
-        case 4:
-            PORTA = aFilter | 0x40;
-            PORTB = bFilter | 0x03;
-            break;
-        case 5:
-            PORTA = aFilter | 0x20;
-            PORTB = bFilter | 0x01;
-            break;
-        case 6:
-            PORTA = aFilter | 0x20;
-            PORTB = bFilter | 0x00;
-            break;
-        case 7:
-            PORTA = aFilter | 0x80;
-            PORTB = bFilter | 0x07;
-            break;
-        case 8:
-            PORTA = aFilter | 0x00;
-            PORTB = bFilter | 0x00;
-            break;
-        case 9:
-            PORTA = aFilter | 0x00;
-            PORTB = bFilter | 0x03;
-            break;
-    }
+void handleDoorOpen() {
+    doorOpen = 1;
 }
 
-
-
-ISR(TIM0_OVF_vect) {
-    if(isDisplayingTensDigit) { // Displaying tens, now display ones
-        isDisplayingTensDigit = 0;
-        PORTA &= ~(1 << PA4);
-        digitToDisplay(tempToDisplay % 10);
-        PORTA |= (1 << PA3);
-    } else { // Displaying ones, now display tens
-        isDisplayingTensDigit = 1;
-        PORTA &= ~(1 << PA3);
-        digitToDisplay(tempToDisplay / 10);
-        PORTA |= (1 << PA4);
-    }
+void handleDoorClose() {
+    doorOpen = 0;
 }
 
-ISR(ADC_vect) {
-    uint8_t low = ADCL;
-    uint16_t adcTemp = ADCH;
-    adcTemp = (adcTemp << 8) | low;
-    //tempToDisplay = (uint8_t)((float)adcTemp / 5.1717 -  72); //1.1v internal ref
-    tempToDisplay = (uint8_t)((float)adcTemp * .1933 - 57); //1.1v internal ref
+void handleInfraredBeamBroken() {
+     beamDetected = 0;
+}
+
+void handleInfraredBeamDetected() {
+     beamDetected = 1;
+}
+
+ISR(PCINT0_vect) {
+    // If door was closed but we're reading door open
+    if(!doorOpen && (PINA & PCINT1)) {
+        handleDoorOpen();
+    // If door was open but we're reading door closed
+    } else if(doorOpen && !(PINA & PCINT1)) {
+        handleDoorClose();
+    }
+
+    // If beam detected and now it is not
+    if(beamDetected && (PINA & PCINT0)) {
+        handleInfraredBeamBroken();
+    // If beam not detected and now it is
+    } else if(!beamDetected && !(PINA & PCINT0)) {
+        handleInfraredBeamDetected();
+    }
 }
 
 int main( void ){
 
-    // Configure 7-seg LED output pins
-    DDRB |= 0x07;
-    PORTB &= ~(0x07);
-    DDRA |= 0xFC;
-    PORTA &= ~(0xFC);
+    PORTA |= (PA1<<1); // Enable pull up resistor for door
 
-    // Configure 7-seg LED 60 Hz strobe
-    TCCR0B |= 0x02; // clk/1 (1000000 hz / 256 / 8 = fast)
-    TIMSK0 |= 0x01; // overflow interrupt enable
-
-
-    // Temperature ADC setup
-    ADMUX |= 0x81; // 10 (internal 1.1v ref) 000001 (ADC1)
-    ADCSRA |= 0x8F; // ADC Enable, ADC Int enable, 128 division
+    GIMSK |= PCIE0; // Enable PCINT 7:0
+    PCMSK0 |= (PCINT0 | PCINT1); // Select which pin interrupt is enabled
 
     sei();
 
-
-
     while(1){ 
-        ADCSRA |= 1 << ADSC;
-        _delay_ms(5000);
+        asm("sleep");
     }
-
-    // avr-gcc -mmcu=attiny84 -DF_CPU=8000000 -O src/main.c -o main
 
 }
