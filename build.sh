5c37bfaa713ba4a0c34f2e539b8a2d704fe37952
Ethan Frei <ethan.frei@gmail.com>
2015-04-22 18:37:51 +0000

    supercharged build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-mkdir bin
+build() {
+    avr-gcc -mmcu=attiny44a -DF_CPU=1000000UL -O -o bin/main.bin src/main.c && \
+    avr-objcopy -O ihex bin/main.bin bin/main.hex
+}
 
-avr-gcc -mmcu=attiny44a -DF_CPU=1000000UL -O -o bin/main.bin src/main.c && \
-avr-objcopy -O ihex bin/main.bin bin/main.hex && \
-avrdude -c usbtiny -p t44 -U flash:w:bin/main.hex
+mkdir -p bin
 
-rm -rf bin
+case "$1" in
+    build)
+        build
+        ;;
+    burn)
+        build && avrdude -c usbtiny -p t44 -U flash:w:bin/main.hex
+        ;;
+
+    fuses)
+        avrdude -c usbtiny -p t44 -U lfuse:w:0x62:m -U hfuse:w:0x5f:m
+        ;;
+    clean)
+        rm -rf bin
+        ;;
+    *)
+        echo "Usage: $0 {build|burn|fuses|clean}"
+        exit 2
+esac
 
 # avrdude -c usbtiny -p t44 -U lfuse:w:0x62:m -U hfuse:w:0x5f:m
