f4845334e9bbc9dd2956518ff4d7965da062a842
Ethan Frei <ethan.frei@gmail.com>
2015-06-15 13:18:17 -0500

    starting servo things

--- src/main.c
+++ src/main.c
@@ -7,16 +7,19 @@
 volatile uint8_t doorOpen = 0;
 volatile uint8_t flagUp = 0;
 volatile uint8_t beamDetected = 0;
-
+volatile uint8_t counter = 0;
 
 void handleDoorOpen() {
     doorOpen = 1;
     PORTA |= (1<<PA3);
+    counter = (counter + 10) % 360;
+    setServoDegrees(counter);
 }
 
 void handleDoorClose() {
     doorOpen = 0;
     PORTA &= ~(1<<PA3);
+    setServoDegrees(0);
 }
 
 void handleInfraredBeamBroken() {
@@ -27,6 +30,11 @@ void handleInfraredBeamBroken() {
 void handleInfraredBeamDetected() {
     beamDetected = 1;
     PORTA &= ~(1<<PA4);
+}
+
+inline void setServoDegrees(uint8_t degree) {
+    uint16_t result = (1000 * (uint16_t)degree) / 180 + 1000;
+    OCR1A = result;
 }
 
 ISR(ANA_COMP_vect) {
@@ -59,15 +67,28 @@ int main( void ){
 
     // LASER SETUP
     ACSR |= (1<<ACIE); // Enable analog comparator interrupt on toggle
+    PORTA |= (1<<PA2);
+
+    // FLAG SETUP
+    ICR1 = 0x4E1F;
+    DDRA |= (1<<PA6);
+    TCCR1A = (1<<COM1A1) | (1<<WGM11);
+    // TCCR1B = (1<<WGM12) | (1<<WGM13) | (1<<CS10);
 
     //Testing things
-    DDRA |= (PA3<<1) | (PA4<<1); // PA3 = Door; PA4 = Laser
-    PORTA |= (1<<PA3) | (1<<PA4);
+    DDRA |= (1<<PA3) | (1<<PA4); // PA3 = Door; PA4 = Laser
+    // PORTA |= (1<<PA3) | (1<<PA4);
     //End Testing things
 
     sei();
 
+    _delay_ms(3000);
     while(1){ 
-        asm("sleep");
+        PORTA |= (1<<PA6);
+        _delay_ms(2);
+        PORTA &= ~(1<<PA6);
+        _delay_ms(18);
+
+        //asm("sleep");
     }
 }
